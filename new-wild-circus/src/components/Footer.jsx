fe97135945c576284ce4b80fd031e8d6f4d0383e
Milan Boisgard <milanboisgard@gmail.com>
2020-01-30 10:51:54 +0100

    footer ok

--- new-wild-circus/src/components/Footer.jsx
+++ new-wild-circus/src/components/Footer.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/footer.css";
 
 function Footer() {
   return (
     <div>
-      <h1>Footer</h1>
+      <div className="container-footer">
+        <div className="container-logo">
+          <h1>Logo</h1>
+        </div>
+        <div className="container-menu-footer">
+          <ul className="menu-footer">
+            <Link to="">
+              <li>Accueil</li>
+            </Link>
+            <Link to="">
+              <li className="item">Tarifs</li>
+            </Link>
+            <Link to="">
+              <li className="item">Blog</li>
+            </Link>
+            <Link to="">
+              <li className="item">Contact</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
